72a3bfdf1ad1d862dee5387b601fb86ec14fc199
RealAce-Git-Hub <tyler.tuazon@gmail.com>
2025-11-22 13:11:05 -0500

    bar plot for accessLevel

--- extraction.py
+++ extraction.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 df = pd.read_csv('data.csv')
 '''print(df)
 print(df.columns)
@@ -12,8 +13,26 @@ print(df.head())
 print(df.describe())'''
 
 print(df['@type'].unique())
-df_new = df.drop(["@type", "conformsTo"],axis=1) 
+keys = ["@type", "conformsTo", "issued"] + ["keyword_" + str(i) for i in range(0,37)]
+print(keys)
+# Only drop columns that actually exist in the DataFrame to avoid KeyError
+keys_to_drop = [k for k in keys if k in df.columns]
+df_new = df.drop(keys_to_drop, axis=1)
 print(df_new)
 
 for col in df_new.columns.tolist()[:5]:
-    print(col,":", df_new[col].unique())
+    print(col, ":", df_new[col].unique())
+
+if 'issued' in df.columns:
+    print(df['issued'].unique())
+else:
+    print("'issued' column not found in dataframe")
+
+print(df_new["accessLevel"].unique())
+print(df_new["accessLevel"].count())
+
+for i in df_new["accessLevel"].unique():
+    print(i, ":", df_new[df_new["accessLevel"] == i].shape[0])
+
+plt.bar(["public", "non-public", "restricted"], [2589, 489, 99])
+plt.show()
